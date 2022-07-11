0f88b5ef1a1bc490a16682c20695967fbe4b4a75
JakubKosowski <desa99@wp.pl>
2022-07-11 17:44:57 +0200

    ..

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,15 +1,15 @@
-            function Transaction (props) {
-                return <span key={props.index}>
-                <p>Nazwa transakcji: {props.name}</p>
-                <p>Wartość w EUR: {props.value}</p>
-                <p>Wartość w PLN: {props.value * props.rate}</p>
-                <button
-                  onClick= {
-                    props.onDelete
-                  }
-                >
-                  Usuń transakcje
-                </button>
-              </span>
-            }
-            export default Transaction;
+function Transaction (props) {
+    return <span key={props.index}>
+    <p>Nazwa transakcji: {props.name}</p>
+    <p>Wartość w EUR: {props.value}</p>
+    <p>Wartość w PLN: {props.value * props.rate}</p>
+    <button
+         onClick= {
+         props.onDelete
+         }
+    >
+        Usuń transakcje
+       </button>
+    </span>
+}
+export default Transaction;
