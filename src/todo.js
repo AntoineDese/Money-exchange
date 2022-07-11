dba3ad8806c6dae5eb248fde30046e8dbc8b6cc0
JakubKosowski <desa99@wp.pl>
2022-07-11 16:22:27 +0200

    names

--- src/todo.js
+++ src/todo.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 
 const Exchange = () => {
-  const [name, setName] = useState("");
-  const [info, setInfo] = useState(0);
+  const [formTransactionName, setFormTransactionName] = useState("");
+  const [rate, setRate] = useState(0);
   const [transaction, setTransaction] = useState([]);
-  const [input, setInput] = useState("");
-  const [output, setOutput] = useState(0);
+  const [formTransactionValue, setFormTransactionValue] = useState("");
+  const [formExchangeValue, setFormExchangeValue] = useState(0);
 
   useEffect(() => {
     fetch("https://api.nbp.pl/api/exchangerates/rates/a/eur/")
@@ -16,31 +16,28 @@ const Exchange = () => {
         throw response;
       })
       .then((data) => {
-        setInfo(data.rates[0].mid);
+        setRate(data.rates[0].mid);
       })
       .catch((error) => {
         console.error("Błąd", error);
       });
   }, []);
 
-  //console.log(info);
-
   const convert = () => {
-    let rate = info;
-    setOutput(input * rate);
+    setFormExchangeValue(formTransactionValue * rate);
     setTransaction((transaction) => [
       ...transaction,
       {
-        name: name,
-        value: input,
+        name: formTransactionName,
+        value: formTransactionValue,
       },
     ]);
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    setInput("");
-    setOutput(0);
-    setName("");
+    setFormTransactionValue("");
+    setFormExchangeValue(0);
+    setFormTransactionName("");
     event.target.reset();
   };
   const deleteTransaction = (index) => {
@@ -59,21 +56,21 @@ const Exchange = () => {
       <div className="main-div">
         <div className="child-div">
           <h1>EURO x PLN</h1>
-          <div className="converter">Przelicznik: 1 EUR = {info} PLN</div>
+          <div className="converter">Przelicznik: 1 EUR = {rate} PLN</div>
           <form onSubmit={handleSubmit}>
             <input
               type="text"
               placeholder="Wpisz wartość w euro"
               onChange={(e) => {
-                setInput(e.target.value);
-                setOutput(e.target.value * info);
+                setFormTransactionValue(e.target.value);
+                setFormExchangeValue(e.target.value * rate);
               }}
-              value={input}
+              value={formTransactionValue}
             />
             <input
               type="text"
               placeholder="Wpisz nazwę transakcji"
-              onChange={(e) => setName(e.target.value)}
+              onChange={(e) => setFormTransactionName(e.target.value)}
             />
             <button
               onClick={() => {
@@ -83,9 +80,10 @@ const Exchange = () => {
               Przelicz
             </button>
           </form>
-          <p>Nazwa transakcji: {name}</p>
+          <p>Nazwa transakcji: {formTransactionName}</p>
           <p>
-            {input ? input : 0} EUR = {output} PLN
+            {formTransactionValue ? formTransactionValue : 0} EUR ={" "}
+            {formExchangeValue} PLN
           </p>
           <div className="transaction">
             <p>Liczba transakcji: {transaction.length}</p>
@@ -97,7 +95,7 @@ const Exchange = () => {
               <span key={index}>
                 <p>Nazwa transakcji: {transaction.name}</p>
                 <p>Wartość w EUR: {transaction.value}</p>
-                <p>Wartość w PLN: {transaction.value * info}</p>
+                <p>Wartość w PLN: {transaction.value * rate}</p>
                 <button
                   onClick={() => {
                     deleteTransaction(index);
